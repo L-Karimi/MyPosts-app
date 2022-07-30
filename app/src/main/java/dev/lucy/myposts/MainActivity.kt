ca2c2a3b53dd5fe98753589819d8f1b127634863
L-Karimi <luciahkei101@gmail.com>
2022-07-30 08:49:19 +0300

    added internet permission

--- app/src/main/java/dev/lucy/myposts/MainActivity.kt
+++ app/src/main/java/dev/lucy/myposts/MainActivity.kt
@@ -2,10 +2,30 @@ package dev.lucy.myposts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun fetchPost(){
+        var apiClient=ApiClient.buildApiClient(ApiInterface::class.java)
+        var request=apiClient.getPosts()
+        request.enqueue(object :Callback<List<Post>>{
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                if (response.isSuccessful){
+                    var posts=response.body()
+                    Toast.makeText(baseContext,"$posts!!.size),posts",Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+            }
+        }
+        )
     }
 }
